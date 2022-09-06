5e37303be941638749bc84c3df59ec03c201b04f
Sara <sara.m.raley@gmail.com>
2022-09-06 11:27:53 -0600

    Add function for a single round

--- rps.js
+++ rps.js
@@ -22,26 +22,38 @@ function getComputerChoice () {
     let result;
     result = Math.floor(Math.random() * 3)
     if (result === 0) {
-        return "Rock";
+        return 'rock';
     } else if (result === 1) {
-        return "Paper";
+        return 'paper';
     } else {
-        return "Scissors";
+        return 'scissors';
     }
 }
 console.log(getComputerChoice ());
 
 
-/* Single round; step 4
+// Single round; step 4
 
 function playRound (playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'rock' || playerSelection === 'paper' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return "Tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats rock";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You win! Rock beats scissors";
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "You lose! Scissors beats paper";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats rock";
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats scissors";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissors beats paper";
+    }
+
 
 }
 
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection);)
-
-
-if (playerSelection === 'rock' && computerSelection === 'rock')
-*/
+console.log(playRound(playerSelection, computerSelection));
