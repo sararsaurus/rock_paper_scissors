35b77d5bc12d3de57f3331e96998fd36cbc00b0c
Sara <sara.m.raley@gmail.com>
2022-09-06 11:08:04 -0600

    Embed if/else into function for computer's random choice generator

--- rps.js
+++ rps.js
@@ -1,5 +1,4 @@
-// Computer's random choice generator
-
+/* Computer's random choice generator; step 3; attempt 1
 function getComputerChoice () {
         return Math.floor(Math.random() * 3);
     };
@@ -15,3 +14,34 @@ if (computerChoice === 0) {
 };
 
 console.log(computerChoice);
+*/
+
+// Computer's random choice generator; step 3; attempt 2
+
+function getComputerChoice () {
+    let result;
+    result = Math.floor(Math.random() * 3)
+    if (result === 0) {
+        return "Rock";
+    } else if (result === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice ());
+
+
+/* Single round; step 4
+
+function playRound (playerSelection, computerSelection) {
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection);)
+
+
+if (playerSelection === 'rock' && computerSelection === 'rock')
+*/
