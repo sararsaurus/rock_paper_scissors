623d2f0c7984d9a27c8a592d8160bdd884c3feb0
Sara <sara.m.raley@gmail.com>
2022-09-07 10:59:21 -0600

    Consolidate the single round of play to add a scoreboard and number of rounds played

--- rps.js
+++ rps.js
@@ -1,22 +1,6 @@
-/* Computer's random choice generator; step 3; attempt 1
-function getComputerChoice () {
-        return Math.floor(Math.random() * 3);
-    };
 
-let computerChoice = getComputerChoice();
 
-if (computerChoice === 0) {
-    computerChoice = "Rock";
-} else if (computerChoice === 1) {
-    computerChoice = "Paper";
-} else {
-    computerChoice = "Scissors"
-};
-
-console.log(computerChoice);
-*/
-
-// Computer's random choice generator; step 3; attempt 2
+// Computer's random choice generator
 
 function getComputerChoice () {
     let result;
@@ -29,12 +13,12 @@ function getComputerChoice () {
         return 'scissors';
     }
 }
-// console.log(getComputerChoice ());
+
+// Single round
 
 
-// Single round; step 4 & 5
-
-function playRound (playerSelection, computerSelection) {
+/* function playRound (playerSelection, computerSelection) {
+    
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock' || playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper' || playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors'){
         return "Tie!";
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
@@ -50,14 +34,92 @@ function playRound (playerSelection, computerSelection) {
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
         return "You win! Scissors beats paper";
     }
-}
-//let playerSelection = 'ROCK';
- 
+} */
 
+//consolidating results and adding a scoreboard
+
+let playerSelection = prompt("Rock, paper, or scissors?");
+let computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 5;
+
+function playRound (playerSelection, computerSelection) {
+
+// Tie
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock' || 
+        playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper' || 
+        playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors'){
+       
+            playerScore += 0;
+            computerScore +=0;
+            roundNumber -= 1;
+            return `Tie!`;
+       
+
+
+// Player wins
+
+        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors' ||
+            playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock' || 
+            playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+            
+            playerScore += 1;
+            roundNumber -= 1;
+            return `You win! ${playerSelection} beats ${computerSelection}.`;
+        
+
+// Computer wins
+
+        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper' || 
+            playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors' || 
+            playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+         
+            computerScore += 1;
+            roundNumber -= 1;
+            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+        }
+
+    }
+    
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`human: ${playerScore}`);
+    console.log(`machine: ${computerScore}`);
+    console.log(`number of games left: ${roundNumber}`)
+   
+
+/*
 // 5-round game; step 6
 
-let playerSelection = 'PAper';
+let playerSelection = prompt("Rock, paper, or scissors?");
 let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
-//playerSelection = playerSelection.toLowerCase();
+
+// console.log(playRound(playerSelection, computerSelection));
+
+function game () {
+    for (let i = 0; i < 5; i++) {
+    console.log(playRound(playerSelection, computerSelection));
+    }
+}
+console.log(game());
+
+// Scoreboard 
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+/* 
+ask user input
+run playRound
+print result
+tabulate player and computer score
+repeat x4
+
+
+*/
+
+
+
